eff360fc5e70e610b5071a9a05be4d050f005848
Emory Merryman <emory.merryman@gmail.com>
2017-10-15 16:59:27 +0000

    cleanup

--- image/root/run.sh
+++ image/root/run.sh
@@ -4,11 +4,11 @@ apk update &&
     apk upgrade &&
     apk add --no-cache bash sudo &&
     adduser -D user &&
-    cp /opt/docker/user.sudo /etc/sudoers.d/user &&
+    cp /opt/docker/sudo.txt /etc/sudoers.d/user &&
     chmod 0444 /etc/sudoers.d/user &&
     mkdir /home/user/bin &&
     cp /opt/docker/docker.sh /home/user/bin/docker &&
     chmod 0500 /home/user/bin/docker &&
-    cp /opt/docker/bash_profile.txt /etc/bashrc &&
-    chown -R user:user /home/user/bin &&
+    cp /opt/docker/bashrc.txt /home/user/.bashrc &&
+    chown -R user:user /home/user/bin /home/user/.bashrc &&
     rm -rf /var/cache/apk/*
