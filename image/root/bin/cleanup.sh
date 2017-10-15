d434e83f6e2cff98a38b4df444b6b9f16aa2e200
Emory Merryman <emory.merryman@gmail.com>
2017-10-15 18:02:26 +0000

    fix cleanup script

--- image/root/bin/cleanup.sh
+++ image/root/bin/cleanup.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 clean(){
-    ls -1 ${HOME}/docker/${1} | while read FILE
+    ls -1 ${HOME}/docker/${1}s | while read FILE
     do
         if [ ${1} == "container" ]
         then
-            docker ${1} stop $(cat ${HOME}/docker/${1}s/${FILE})
+            docker ${1} stop $(cat ${HOME}/docker/${1}s/${FILE}) &&
+                docker ${1} rm --volumes $(cat ${HOME}/docker/${1}s/${FILE})
+        else
+            docker ${1} rm $(cat ${HOME}/docker/${1}s/${FILE})
         fi &&
-            docker ${1} rm --volumes $(cat ${HOME}/docker/${1}s/${FILE}) &&
             rm -f ${HOME}/docker/${1}s/${FILE}
     done
 } &&
