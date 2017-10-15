587ef1db43990dd62bc95703a5f57df297b4fab2
Emory Merryman <emory.merryman@gmail.com>
2017-10-15 17:38:27 +0000

    create bin directory

--- image/root/bin/sshd.sh
+++ image/root/bin/sshd.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-if [ ! -f ${HOME}/docker/volumes/sshd-config ]
+if [ ! -f /home/user/docker/networks/system ]
 then
-    docker volume create > ${HOME}/docker/volumes/sshd-config &&
-        cat \
-            /opt/docker/etc/sshd_config.txt | docker \
-            container \
-            run \
-            --interactive \
-            --rm \
-            --mount type=volume,source=$(cat ${HOME}/docker/volumes/sshd_config),destination=/etc/ssh \
-            --workdir /etc/ssh \
-            alpine:3.4 \
-                tee sshd_config
+    docker network create $(uuidgen) > /home/user/docker/networks/system
 fi &&
+if [ ! -f ${HOME}/docker/volumes/sshd-config ]
+    then
+        docker volume create > ${HOME}/docker/volumes/sshd-config &&
+            cat \
+                /opt/docker/etc/sshd_config.txt | docker \
+                container \
+                run \
+                --interactive \
+                --rm \
+                --mount type=volume,source=$(cat ${HOME}/docker/volumes/sshd_config),destination=/etc/ssh \
+                --workdir /etc/ssh \
+                alpine:3.4 \
+                    tee sshd_config
+    fi &&
     if [ ! -f ${HOME}/docker/volumes/sshd_dot_ssh ]
     then
         docker volume create > ${HOME}/docker/volumes/sshd-dot_ssh &&
--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-if [ ! -f /home/user/docker/networks/system ]
-then
-    docker volume create $(uuidgen) > /home/user/docker/networks/system
-fi &&
 bash
