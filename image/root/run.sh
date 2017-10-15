7fc8d74d68e4997b8816342b7c369b67fed24019
Emory Merryman <emory.merryman@gmail.com>
2017-10-15 16:15:28 +0000

    add base sudo

--- image/root/run.sh
+++ image/root/run.sh
@@ -2,6 +2,8 @@
 
 apk update &&
     apk upgrade &&
-    apk add --no-cache bash &&
+    apk add --no-cache bash sudo &&
     adduser -D user &&
+    cp /opt/docker/user.sudo /etc/sudoers.d/user &&
+    chmod 0444 /etc/sudoers.d/user &&
     rm -rf /var/cache/apk/*
