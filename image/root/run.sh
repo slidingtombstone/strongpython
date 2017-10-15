2b9ac20be0317194ee1efa23a90419c4c981af0f
Emory Merryman <emory.merryman@gmail.com>
2017-10-15 16:57:26 +0000

    use /etc/bashrc

--- image/root/run.sh
+++ image/root/run.sh
@@ -9,6 +9,6 @@ apk update &&
     mkdir /home/user/bin &&
     cp /opt/docker/docker.sh /home/user/bin/docker &&
     chmod 0500 /home/user/bin/docker &&
-    cp /opt/docker/bash_profile.txt /home/user/.bashrc &&
-    chown -R user:user /home/user/bin /home/user/.bashrc &&
+    cp /opt/docker/bash_profile.txt /etc/bashrc &&
+    chown -R user:user /home/user/bin &&
     rm -rf /var/cache/apk/*
