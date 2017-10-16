789c34945773256694e75152879a08cc2a8755ae
Emory Merryman <emory.merryman@gmail.com>
2017-10-16 02:25:50 +0000

    change docker image

--- image/root/bin/sshd.sh
+++ image/root/bin/sshd.sh
@@ -26,7 +26,7 @@ if [ ! -f ${HOME}/docker/volumes/sshd_config ]
             --restart always \
             --cidfile ${HOME}/docker/containers/sshd \
             --mount type=volume,source=$(cat ${HOME}/docker/volumes/sshd_config),destination=/etc/ssh \
-            rastasheep/ubuntu-sshd:14.04 \
+            panubo/sshd:14.04 \
                 /usr/sbin/sshd \
                 -D \
                 -f /etc/ssh/sshd_config &&
