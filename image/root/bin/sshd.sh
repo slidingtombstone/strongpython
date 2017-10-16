8b0089c6cedbd339d8778ca7f2471d8dddbe76f4
Emory Merryman <emory.merryman@gmail.com>
2017-10-16 02:35:25 +0000

    sleep 10s

--- image/root/bin/sshd.sh
+++ image/root/bin/sshd.sh
@@ -32,7 +32,7 @@ if [ ! -f ${HOME}/docker/volumes/sshd_config ]
                 -f /etc/ssh/sshd_config &&
             docker network connect --alias sshd $(cat ${HOME}/docker/networks/system) $(cat ${HOME}/docker/containers/sshd) &&
             docker container start $(cat ${HOME}/docker/containers/sshd) &&
-            echo ALPHA 00300 &&
+            sleep 10s &&
             docker \
                 container \
                 exec \
@@ -42,7 +42,6 @@ if [ ! -f ${HOME}/docker/volumes/sshd_config ]
                 $(cat ${HOME}/docker/containers/sshd) \
                     touch \ 
                     /root/.ssh/authorized_keys &&
-            echo ALPHA 00400 &&
             docker \
                 container \
                 exec \
