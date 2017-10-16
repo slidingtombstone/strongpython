b348049e9f73a7ece0cc65fc71fb1ed283103488
Emory Merryman <emory.merryman@gmail.com>
2017-10-16 14:55:23 +0000

    create system network

--- image/root/bin/browser.sh
+++ image/root/bin/browser.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-docker \
-    container \
-    create \
-    --cidfile ${HOME}/docker/containers/browser \
-    --env DISPLAY=${DISPLAY} \
-    --volume /tmp/.X11-unix:/tmp/.X11-unix:ro \
-    slidingtombstone/browser:29d10b97bdde3a2b2d505474f51d97b2830b6120 &&
+create-system-network &&
+    docker \
+        container \
+        create \
+        --cidfile ${HOME}/docker/containers/browser \
+        --env DISPLAY=${DISPLAY} \
+        --volume /tmp/.X11-unix:/tmp/.X11-unix:ro \
+        slidingtombstone/browser:29d10b97bdde3a2b2d505474f51d97b2830b6120 &&
     docker network connect $(cat ${HOME}/docker/networks/system) $(cat ${HOME}/docker/containers/browser) &&
     docker container start ${HOME}/docker/containers/browser
--- image/root/bin/sshd.sh
+++ image/root/bin/sshd.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-if [ ! -f /home/user/docker/networks/system ]
-then
-    docker network create $(uuidgen) > /home/user/docker/networks/system
-fi &&
-if [ ! -f ${HOME}/docker/volumes/sshd_config ]
-    then
-        docker volume create > ${HOME}/docker/volumes/sshd_config &&
-            cat \
-                /opt/docker/etc/sshd_config.txt | docker \
-                container \
-                run \
-                --interactive \
-                --rm \
-                --mount type=volume,source=$(cat ${HOME}/docker/volumes/sshd_config),destination=/etc/ssh \
-                --workdir /etc/ssh \
-                alpine:3.4 \
-                    tee sshd_config
-    fi &&
+create-system-network &&
+    if [ ! -f ${HOME}/docker/volumes/sshd_config ]
+        then
+            docker volume create > ${HOME}/docker/volumes/sshd_config &&
+                cat \
+                    /opt/docker/etc/sshd_config.txt | docker \
+                    container \
+                    run \
+                    --interactive \
+                    --rm \
+                    --mount type=volume,source=$(cat ${HOME}/docker/volumes/sshd_config),destination=/etc/ssh \
+                    --workdir /etc/ssh \
+                    alpine:3.4 \
+                        tee sshd_config
+        fi &&
     if [ ! -f ${HOME}/docker/containers/sshd ]
     then
         docker \
