d7244f9c289a02659b02adc4649596c3caf717b0
Emory Merryman <emory.merryman@gmail.com>
2017-10-16 14:38:39 +0000

    use newer browser

--- image/root/bin/browser.sh
+++ image/root/bin/browser.sh
@@ -6,6 +6,6 @@ docker \
     --cidfile ${HOME}/docker/containers/browser \
     --env DISPLAY=${DISPLAY} \
     --volume /tmp/.X11-unix:/tmp/.X11-unix:ro \
-    slidingtombstone/browser:976e5f4d1a95113ae2afcaf5adc733d89728e4d8 &&
+    slidingtombstone/browser:29d10b97bdde3a2b2d505474f51d97b2830b6120 &&
     docker network connect $(cat ${HOME}/docker/networks/system) $(cat ${HOME}/docker/containers/browser) &&
     docker container start ${HOME}/docker/containers/browser
