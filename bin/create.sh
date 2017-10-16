e4c72dd5af81bc5a1ba77096983d55682d457163
Emory Merryman <emory.merryman@gmail.com>
2017-10-16 11:39:53 +0000

    add keys

--- bin/create.sh
+++ bin/create.sh
@@ -8,6 +8,8 @@ docker \
     --volume /var/run/docker.sock:/var/run/docker.sock:ro \
     --env USER_NAME \
     --env USER_EMAIL \
+    --env GITHUB_ID_RSA="$(cat ${HOME}/.ssh/id_rsa)" \
+    --env KNOWN_HOSTS="$(cat ${HOME}/.ssh/known_hosts)" \
     --restart always \
     slidingtombstone/strongpython:$(git rev-parse --verify HEAD) &&
     docker container start strongpython
