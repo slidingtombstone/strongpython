19aa9b22b13124eccfbc335b83ed4feeb8e0cdff
Emory Merryman <emory.merryman@gmail.com>
2017-10-16 15:00:21 +0000

    fixed browser

--- image/root/bin/browser.sh
+++ image/root/bin/browser.sh
@@ -9,4 +9,4 @@ create-system-network &&
         --volume /tmp/.X11-unix:/tmp/.X11-unix:ro \
         slidingtombstone/browser:29d10b97bdde3a2b2d505474f51d97b2830b6120 &&
     docker network connect $(cat ${HOME}/docker/networks/system) $(cat ${HOME}/docker/containers/browser) &&
-    docker container start ${HOME}/docker/containers/browser
+    docker container start $(cat ${HOME}/docker/containers/browser)
